c656ccbfbf8a71ec659e0f7eab6b6b477323a4fc
czq <1772398186@qq.com>
2021-12-24 21:41:59 +0800

    行就是行~~~

---
+++ src/main/java/com/example/sgqservice/SgqServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.sgqservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgqServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SgqServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sgqservice/SgqServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sgqservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgqServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
